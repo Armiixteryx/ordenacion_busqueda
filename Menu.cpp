fb6f3c9aa2cea4f24126be762fc299fea1bdb0ee
David Camperos <camperos.davidjosue@gmail.com>
2021-07-31 09:55:06 -0400

    feat: implementar menu principal

--- Menu.cpp
+++ Menu.cpp
@@ -8,19 +8,30 @@ Menu::Menu() {
 }
 
 int Menu::menu_principal() {
-	cout << "Ordenacion y busqueda" << endl;
-	Utils::imprimir_separador('=');
-	cout << "1. Listar libros." << endl;
-	cout << "2. Cargar libros en RAM." << endl;
-	cout << "3. Ordenar libros en RAM." << endl;
-	cout << "4. Ordenar libros en archivo." << endl;
-	cout << "5. Busqueda binaria en libros en RAM." << endl;
-	cout << "6. Busqueda secuencial en libros en archivo." << endl;
-	cout << "7. Salir." << endl;
-	cout << "Su opcion: ";
-	
-	//int opcion = opcion
+	int opcion = 0;
 
-	return 0;
+	do {
+		system("cls");
+		cout << "Ordenacion y busqueda" << endl;
+		Utils::imprimir_separador('=');
+		cout << "1. Listar libros." << endl;
+		cout << "2. Cargar libros en RAM." << endl;
+		cout << "3. Ordenar libros en RAM." << endl;
+		cout << "4. Ordenar libros en archivo." << endl;
+		cout << "5. Busqueda binaria en libros en RAM." << endl;
+		cout << "6. Busqueda secuencial en libros en archivo." << endl;
+		cout << "7. Salir." << endl;
+		cout << "Su opcion (1-7): ";
+		
+		cin >> opcion;
+		cin.ignore();
+
+		if (opcion < MENU_PRINCIPAL_LISTAR || opcion > MENU_PRINCIPAL_SALIR) {
+			cout << "Opcion incorrecta. Intente nuevamente." << endl;
+			system("pause");
+		}
+	} while (opcion < MENU_PRINCIPAL_LISTAR || opcion > MENU_PRINCIPAL_SALIR);
+
+	return opcion;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include "Menu.h"
 
+using namespace std;
+
 int main(int argc, char** argv) {
 	Menu menu;
 	
-	int opcion_menu = menu.menu_principal();
+	int opcion_menu = 0;
+	
+	while (opcion_menu != MENU_PRINCIPAL_SALIR) {
+		opcion_menu = menu.menu_principal();
+		switch (opcion_menu) {
+			case MENU_PRINCIPAL_LISTAR:
+				break;
+			case MENU_PRINCIPAL_CARGAR_RAM:
+				break;
+			case MENU_PRINCIPAL_ORDENAR_RAM:
+				break;
+			case MENU_PRINCIPAL_ORDENAR_ARCHIVO:
+				break;
+			case MENU_PRINCIPAL_BUSQ_BIN_RAM:
+				break;
+			case MENU_PRINCIPAL_BUSQ_SEQ_ARCHIVO:
+				break;
+			case MENU_PRINCIPAL_SALIR:
+				break;
+		}
+	}
+
 	return 0;
 }
 
